df4c8fa79d0099539073b42b95e904302aea2373
Chuma Okoro <chuma.okoro76@gmail.com>
2018-10-30 12:29:15 -0400

    Update documentation

--- PIDmanager/src/Test.java
+++ PIDmanager/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		Application app = new Application();
 		PIDThread thread;
-		for (int i=0; i <200; i ++) {
+		for (int i=0; i <10; i ++) {
 			 thread= new PIDThread(app);
 			 thread.start();
 		}
